6a7aef18e076734b0f223c1f5ab84b4b7225e8e0
Daniel Sones <Daniel.sones@btinternet.com>
2020-05-08 17:14:20 +0100

    Create Game and window classse
    So far only a red rectangle is displayed to the JFrame. THERE IS NO GAMEPLAY

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,88 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-public class game {
-}
+public class Game extends Canvas implements Runnable{
+
+    private Boolean isRunning = false;
+    private Thread thread;
+
+    public Game(){
+        new Window(1000, 563, "Wizard Shooter", this);
+        start();
+
+    }
+
+    private void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }//Start end
+
+    private void stop(){
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }//Stop end
+
+    public static void main(String[] args){
+        new Game();
+    }
+
+    @Override
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) /ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                //updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frames = 0;
+                //updates = 0;
+            }
+        }
+        stop();
+    }//run
+
+    private void render() {
+        //Make a buffer strategy - preloads frames before they're shown to the user
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3); // What ever is inside the parenthesis is the number that are preloaded
+            //three suposedely offers the best peformance
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        ////////////////////////////////////////
+        // Anything between these comments is shown to the screen
+        g.setColor(Color.red);
+        g.fillRect(0, 0, 1000, 563);
+
+        ////////////////////////////////////////
+        g.dispose();
+        bs.show();
+
+    }// render
+
+    private void tick() {
+    }
+
+
+}//class
--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-public class window {
+public class Window {
+    public Window(int width, int height, String title, Game game){
+        //Create A JFrame
+        JFrame frame = new JFrame(title);
+        //Setup frame size
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        //Add the game method to the JFrame
+        frame.add(game);
+        //make the window un-resizable
+        frame.setResizable(false);
+        //Close the window when X is pressed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Start the frame in the very center of the window
+        frame.setLocationRelativeTo(null);
+        //Make the window visible
+        frame.setVisible(true);
+
+    }
 }
