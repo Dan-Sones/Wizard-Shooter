aa00a87c4df19af92840135cfa60990e26424d8a
Daniel Sones <Daniel.sones@btinternet.com>
2020-05-08 21:11:04 +0100

    Working camera and map!

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,26 @@
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
     private Boolean isRunning = false;
     private Thread thread;
     private Handler handler;
+    private BufferedImage sprite_sheet = null;
+    private BufferedImage level = null;
+    private Camera camera;
 
     public Game() {
         new Window(1000, 563, "Wizard Shooter", this);
         start();
 
         handler = new Handler();
+        camera = new Camera(0, 0);
         this.addKeyListener(new KeyInput(handler));
-
-        handler.addObject(new Wizard(100, 100, ID.Player, handler));
+        BufferedImageLoader loader = new BufferedImageLoader();
+        level = loader.loadImage("map.png");
+        loadLevel(level);
 
 
     }
@@ -77,21 +83,79 @@ public class Game extends Canvas implements Runnable {
             return;
         }
         Graphics g = bs.getDrawGraphics();
+        Graphics2D g2d = (Graphics2D) g;
         ////////////////////////////////////////
         // Anything between these comments is shown to the screen
         g.setColor(Color.red);
         g.fillRect(0, 0, 1000, 563);
+        g2d.translate(-camera.getX(), -camera.getY());
 
-        handler.render(g); //MUST BE AT THE BOTTOM!!!!!
+
+
+        handler.render(g);
+        g2d.translate(camera.getX(), camera.getY());
         ////////////////////////////////////////
         g.dispose();
         bs.show();
 
     }// render
 
+    //loading the level
+    private void loadLevel (BufferedImage image){
+        int w = image.getWidth();
+        int h = image.getHeight();
+
+        for (int xx = 0; xx < w; xx++){
+            for(int yy = 0; yy < h; yy++ ){
+                int pixel = image.getRGB(xx, yy);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                if(red == 255){
+                    handler.addObject(new Block(xx*32, yy*32, ID.Block));
+                }
+                if(blue == 255){
+                    handler.addObject(new Wizard(xx*32, yy*32, ID.Player, handler));
+                }
+            }
+        }
+    }
+
     private void tick() {
+        for(int i = 0; i < handler.object.size(); i++){
+            if(handler.object.get(i).getId() == ID.Player){
+                camera.tick(handler.object.get(i));
+            }
+        }
+
         handler.tick();
     }
 
 
 }//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
